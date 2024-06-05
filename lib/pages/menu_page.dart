fedad05a0cf4072e2d9f107085a7b0ed337d76a4
Manuherrera <esteban.manuel06@gmail.com>
2024-06-05 01:22:20 -0300

    modifique un detalle de los botones

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -5,8 +5,8 @@ import 'package:flutter/cupertino.dart';
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Set<String> dificultad1 = {};
-    Set<String> dificultad2 = {};
+    var dificultad1 ;
+    var dificultad2 ;
 
     return Scaffold(
       appBar: AppBar(
@@ -21,25 +21,25 @@ class MenuPage extends StatelessWidget {
             SizedBox(height: 20),
             CupertinoSlidingSegmentedControl(
               children: {
-                'facil': Text('Facil', style: TextStyle(fontSize: 16.0)),
-                'medio': Text('Medio', style: TextStyle(fontSize: 16.0)),
-                'dificil': Text('Dificil', style: TextStyle(fontSize: 16.0)),
-                'experto': Text('Experto', style: TextStyle(fontSize: 16.0)),
+                0: Text('Facil', style: TextStyle(fontSize: 16.0)),
+                1: Text('Medio', style: TextStyle(fontSize: 16.0)),
+                2: Text('Dificil', style: TextStyle(fontSize: 16.0)),
+                3: Text('Experto', style: TextStyle(fontSize: 16.0)),
               },
-              onValueChanged: (value) { },
-              groupValue: dificultad1.isNotEmpty ? dificultad1.first : null,
+              onValueChanged: (value) {dificultad1 = value;},
+              groupValue: dificultad1 = 0,
             ),
             SizedBox(height: 30),
             CupertinoSlidingSegmentedControl(
               children: {
-                'Animales': Text('Animales', style: TextStyle(fontSize: 15.0)),
-                'Paises': Text('Paises', style: TextStyle(fontSize: 15.0)),
-                'Objetos': Text('Objetos', style: TextStyle(fontSize: 15.0)),
-                'Futbol': Text('Futbol', style: TextStyle(fontSize: 15.0)),
-                'Gerenal': Text('Gerenal', style: TextStyle(fontSize: 15.0)),
+                0: Text('Animales', style: TextStyle(fontSize: 15.0)),
+                1: Text('Paises', style: TextStyle(fontSize: 15.0)),
+                2: Text('Objetos', style: TextStyle(fontSize: 15.0)),
+                3: Text('Futbol', style: TextStyle(fontSize: 15.0)),
+                4: Text('Gerenal', style: TextStyle(fontSize: 15.0)),
               },
-              onValueChanged: (value) { },
-              groupValue: dificultad2.isNotEmpty ? dificultad2.first : null,
+              onValueChanged: (value) {dificultad2 = value;},
+              groupValue: dificultad2 = 0,
             ),
             SizedBox(height: 40),
             ElevatedButton(
