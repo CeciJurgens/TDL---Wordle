5c363042de55848f7cdb108b99cdae95b31f3218
Ceci Jurgens <cjurgens@fi.uba.ar>
2024-06-19 05:18:34 -0300

    hint + dark mode + rules page

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,3 +4,6 @@ const correctGreen = Color.fromARGB(255, 107, 170, 100);
 const containsYellow = Color.fromARGB(255, 201, 180, 87);
 const lightThemeLightShade = Color.fromARGB(255, 211, 214, 215);
 const lightThemeDarkShade = Color.fromARGB(255, 120, 124, 127);
+
+const darkThemeLightShade = Color.fromARGB(255, 60, 63, 65);
+const darkThemeDarkShade = Color.fromARGB(255, 30, 33, 35);
